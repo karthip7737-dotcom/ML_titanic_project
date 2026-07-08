25a78910a16d11a6dba6e93c717463f6136cc694
karthip7737-dotcom <karthip7737@gmail.com>
2026-07-08 11:29:21 +0530

    Update schemas.py

--- schemas.py
+++ schemas.py
@@ -1,11 +1,8 @@
 from typing import Optional
-
 from pydantic import BaseModel
 
-class passenger(BaseModel):
-
-    pclass: int
-    sex: str
-
-    age: Optional[float]=None
-    fare: Optional[float]=None
+class Passenger(BaseModel):
+    pclass:int
+    sex:str
+    age:Optional[float]=None
+    fare:Optional[float]=None
